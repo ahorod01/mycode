f28e31a4de683825d3e4ad69b91725bdb68ca1da
ahorod01 <ahorod@gmail.com>
2026-08-25 13:21:44 -0700

    Delete blocksiteviahosts.py

--- blocksiteviahosts.py
+++
@@ -1,8 +0,0 @@
-import os
-
-sites=["facebook.com","youtube.com"]
-hosts="C:\\windows\\\system32\\drivers\\etc\\hosts"
-
-for site in sites:
-    open(hosts,"a").write(f"\n172.0.0.1 {site}")
-    print(f"Blocked {site}")
